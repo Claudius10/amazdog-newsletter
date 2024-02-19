8d43a43645ebd7cc98e7dcef271d079803d189b5
Claudius10 <66148715+Claudius10@users.noreply.github.com>
2024-02-19 19:35:51 +0100

    added db data

---
+++ src/main/resources/db_data/chartssql.sql
@@ -0,0 +1,3 @@
+INSERT INTO railway.charts (id, title, type) VALUES (9, 'Sacrificio Mundial de Animales', 'Bar');
+INSERT INTO railway.charts (id, title, type) VALUES (10, 'Sacrificio Mundial de Animales', 'Line');
+INSERT INTO railway.charts (id, title, type) VALUES (13, '¿A dónde van los perros y gatos que llegan a un refugio de animales? (2022))', 'Doughnut');
---
+++ src/main/resources/db_data/subjectssql.sql
@@ -0,0 +1,9 @@
+INSERT INTO railway.subjects (id, name, chart_id) VALUES (14, 'Sacrificio Mundial de Animales, Vacas', 9);
+INSERT INTO railway.subjects (id, name, chart_id) VALUES (15, 'Sacrificio Mundial de Animales, Pollos', 9);
+INSERT INTO railway.subjects (id, name, chart_id) VALUES (16, 'Sacrificio Mundial de Animales, Cerdos', 9);
+INSERT INTO railway.subjects (id, name, chart_id) VALUES (17, 'Sacrificio Mundial de Animales, Ovejas', 9);
+INSERT INTO railway.subjects (id, name, chart_id) VALUES (18, 'Sacrificio Mundial de Animales, Vacas', 10);
+INSERT INTO railway.subjects (id, name, chart_id) VALUES (19, 'Sacrificio Mundial de Animales, Pollos', 10);
+INSERT INTO railway.subjects (id, name, chart_id) VALUES (20, 'Sacrificio Mundial de Animales, Cerdos', 10);
+INSERT INTO railway.subjects (id, name, chart_id) VALUES (21, 'Sacrificio Mundial de Animales, Ovejas', 10);
+INSERT INTO railway.subjects (id, name, chart_id) VALUES (24, '¿A dónde van los perros y gatos que llegan a un refugio de animales?', 13);
